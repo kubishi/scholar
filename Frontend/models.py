cf07ea313657dd02afc779d1a81547f47000dc5b
Sebastian Vahe Lange <143560805+MasterSebas04@users.noreply.github.com>
2025-10-08 14:57:52 -0700

    General fixes, review page works better. Admin privelages.

--- Frontend/models.py
+++ Frontend/models.py
@@ -4,6 +4,7 @@ class User(db.Model):
     google_auth_id = db.Column(db.String(60), primary_key=True)
     user_name = db.Column(db.String(50))
     user_email = db.Column(db.String(50))
+    user_privelages = db.Column(db.String(50))
 
 class Favorite_Conf(db.Model):
     user_id = db.Column(db.String(60), db.ForeignKey('user.google_auth_id'), primary_key=True)
